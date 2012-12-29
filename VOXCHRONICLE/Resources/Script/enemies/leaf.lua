44eda1751b81c75812c8f8fead9532778200f44a
giginet <giginet.net@gmail.com>
2012-12-29 15:40:09 +0900

    敵モンスター追加した

---
+++ VOXCHRONICLE/Resources/Script/enemies/leaf.lua
@@ -0,0 +1,16 @@
+Enemy = {
+  name = "はっぱ",
+  imageName = "leaf",
+  level = 1,
+  hp = 1,
+  exp = 5,
+  attack = 1,
+  counter = 1,
+  speed = 2,
+  item = EnemyItemNone,
+  animationFrames = 4,
+  type = SkillTypeNormal,
+  choiceSkill = function(self)
+    return nil
+  end
+}
