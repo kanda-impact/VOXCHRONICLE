b81cb05e6af0fa70d61eab17a3470e9ef8ff0ed2
giginet <giginet.net@gmail.com>
2012-12-30 18:55:11 +0900

    ステージラベル差し替え。トラックキャッシュの枠組みを作成

---
+++ VOXCHRONICLE/Classes/VISS/TrackManager.cpp
@@ -0,0 +1,21 @@
+//
+//  TrackManager.cpp
+//  VOXCHRONICLE
+//
+//  Created by giginet on 2012/12/30.
+//
+//
+
+#include "TrackManager.h"
+
+static void preloadMusics(const char* prefix, const char* list) {
+}
+
+static void preloadSE(const char* list) {
+}
+
+static void purgeMusics() {
+}
+
+static void purgeSE() {
+}
---
+++ VOXCHRONICLE/Classes/VISS/TrackManager.h
@@ -0,0 +1,14 @@
+//
+//  TrackManager.h
+//  VOXCHRONICLE
+//
+//  Created by giginet on 2012/12/30.
+//
+//
+
+#ifndef __VOXCHRONICLE__TrackManager__
+#define __VOXCHRONICLE__TrackManager__
+
+#include <iostream>
+
+#endif /* defined(__VOXCHRONICLE__TrackManager__) */
