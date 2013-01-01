047d409ea0fec3bd6e16b97074c9f133f7e86c5c
giginet <giginet.net@gmail.com>
2013-01-01 23:58:54 +0900

    11面以降に敵がでないの修正？

--- VOXCHRONICLE/Resources/Script/map/test.lua
+++ VOXCHRONICLE/Resources/Script/map/test.lua
@@ -10,13 +10,8 @@ Map = {
     return {slime = 5, leaf = 5, typhoon = 5, geek_shield = 2, tnt = 3, ginet = 2}
   end,
   getEnemyPopRate = function(level)
-    if level <=5 then
-      return 0.6
-    end
     return 0.7
   end,
   fixedEnemies = {
-    {"ginet", 100},
-    {"tnt", 20}
   }
 }
