552bff3ac2fe25439e4d3974f94bda2db2b987ea
giginet <giginet.net@gmail.com>
2013-01-04 00:50:07 +0900

    旧敵キャラ追加した

---
+++ VOXCHRONICLE/Resources/Script/map/proto0.lua
@@ -0,0 +1,39 @@
+Map = {
+  name = "スクリプトテスト",
+  wayMusic = "dub",
+  bossMusic = "boss",
+  backgroundImage = "",
+  nextMaps = {"test"},
+  initialLevel = 1,
+  maxLevel = 10,
+  getEnemyTable = function(level)
+    if level <=1 then
+      return {geek_shield = 1, slime = 2, leaf = 5, typhoon = 2}
+    elseif level <=3 then
+      return {geek_shield = 1, slime = 2, leaf = 5, typhoon = 2}
+    elseif level <=5 then
+      return {geek_shield = 1, slime = 2, leaf = 5, typhoon = 2}
+    elseif level <=7 then
+      return {geek_shield = 1, slime = 2, leaf = 5, typhoon = 2}
+    elseif level == 10 then
+      return {}
+    end
+    return {}
+  end,
+  onLevel = function(level, characterManager, enemyManager)
+    if level == 10 then
+      enemyManager:popEnemyAt("knight", MAX_ROW - 1, 1)
+    end
+  end,
+  getEnemyPopRate = function(level)
+    if level <=5 then
+      return 0.3
+    end
+    return 0.5
+  end,
+  fixedEnemies = {
+    {"ginet", 100},
+    {"tnt", 20},
+    {"Level10", 10}
+  }
+}
