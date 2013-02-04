a1609ee0d4df0d9b794dfc273dafbcd175f4c51a
giginet <giginet.net@gmail.com>
2013-02-04 22:25:54 +0900

    ジーク面から蝶を消した

--- VOXCHRONICLE/Resources/Script/map/1b.lua
+++ VOXCHRONICLE/Resources/Script/map/1b.lua
@@ -10,14 +10,11 @@ Map = {
     if level <= 5 then
       return {slime = 5, leaf = 5, typhoon = 5, geek_shield = 2, tnt = 3, ginet = 2}
     else
-      return {moth = 1}
+      return {dragon = 1}
     end
   end,
   getEnemyPopRate = function(level)
-    if level <= 5 then
-      return 0.7
-    end
-    return 1.0
+    return 0.7
   end,
   fixedEnemies = {
   }
