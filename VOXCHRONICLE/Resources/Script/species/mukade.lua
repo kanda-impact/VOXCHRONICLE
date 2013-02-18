eaf44dbdddaa2935f31366b660d12282f5350f5c
giginet <giginet.net@gmail.com>
2013-02-18 19:23:14 +0900

    敵キャラムカデ追加。蛾の4コマ目差し替え

---
+++ VOXCHRONICLE/Resources/Script/species/mukade.lua
@@ -0,0 +1,15 @@
+Enemy = {
+  name = "ムカデ",
+  imageName = "mukade",
+  attack = 1,
+  baseExp = 1,
+  hasFrame = true,
+  counter = 1,
+  speed = 2,
+  animationFrames = 4,
+  performSkill = function(self)
+  return ""
+  end
+}
+
+return Enemy
