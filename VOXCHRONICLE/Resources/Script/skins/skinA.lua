bf5ff091309ca657d37d0440123fc16c0c149daf
wwwpaper <oniok@hotmail.co.jp>
2013-02-21 19:26:11 +0900

    スキン１のHP表示位置の調整

--- VOXCHRONICLE/Resources/Script/skins/skinA.lua
+++ VOXCHRONICLE/Resources/Script/skins/skinA.lua
@@ -7,7 +7,7 @@ Skin = {
   buildSkin = function(self)
     -- ここでスキンの定義をｇｄｇｄ書きます
     local hpLabel = CCSprite:create("cyber_hp.png")
-    hpLabel:setPosition(ccp(20, 308));
+    hpLabel:setPosition(ccp(20, 300));
     self:addChild(hpLabel)
     
     local mpLabel = CCSprite:create("cyber_mp.png");
@@ -16,18 +16,18 @@ Skin = {
   
     local currentHPLabel = CCLabelAtlas:create("0", "cyber_hp_numbers.png", 17, 18, string.byte("0"))
     currentHPLabel:setScale(0.6)
-    currentHPLabel:setPosition(ccp(169, 308))
+    currentHPLabel:setPosition(ccp(49, 293))
     currentHPLabel:setAnchorPoint(ccp(0.5, 0.5))
   
     local maxHPLabel = CCLabelAtlas:create("0", "cyber_hp_numbers.png", 17, 18, string.byte("0"))
     maxHPLabel:setScale(0.4)
-    maxHPLabel:setPosition(ccp(205, 305))
+    maxHPLabel:setPosition(ccp(85, 290))
     maxHPLabel:setAnchorPoint(ccp(0.5, 0.5))
     self:addChild(currentHPLabel, 0, CurrentHPLabelTag)
     self:addChild(maxHPLabel, 0, MaxHPLabelTag)
   
     local slash = CCSprite:create("cyber_slash.png")
-    slash:setPosition(ccp(191, 308))
+    slash:setPosition(ccp(71, 293))
     self:addChild(slash)
   
     local stageLabel = CCSprite:create("cyber_level.png")
