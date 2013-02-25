4fb408f92c3e66d83c39dd6c6e674ae54f466b85
giginet <giginet.net@gmail.com>
2013-02-25 19:26:24 +0900

    ナイトの技によってリフを変更するようにした

--- VOXCHRONICLE/Resources/Script/species/knight.lua
+++ VOXCHRONICLE/Resources/Script/species/knight.lua
@@ -5,7 +5,7 @@ Enemy = {
   attack = 1,
   baseExp = 1,
   hasFrame = true,
-  counter = 1,
+  counter = 0,
   getSpeed = function(enemy, characterManager)
     local row = enemy:getRow()
     if row < 4 then -- 4列目より前に行ったとき、速度を0にします
@@ -20,15 +20,19 @@ Enemy = {
   animationFrames = 4,
   performSkill = function(self)
     local isCharge = self:getRegister("swordTurn", 0) > 0 -- チャージ中かどうか
+    self:setCounter(-1)
     if self:getRow() < 4 then -- 最前列にいるとき
       if isCharge then -- チャージ中だったら常にswordを返してやる
+        self:setCounter(2)
         return "sword"
       else -- それ以外は乱数によって行動を振り分ける
         math.random(100)
         rand = math.random(100)
         if rand <= 70 then -- 70%で鉄球投げ
+          self:setCounter(1)
           return "ironball"
         elseif rand <= 90 then -- 残り10%で溜め攻撃
+          self:setCounter(2)
           return "sword"
         end
       end
