ee4a821805561f5ae904f3a1ccb0c59a7a619402
giginet <giginet.net@gmail.com>
2013-03-13 21:34:24 +0900

    いてつくはどうの演出を強化

--- VOXCHRONICLE/Resources/Script/enemySkills/reset_tension.lua
+++ VOXCHRONICLE/Resources/Script/enemySkills/reset_tension.lua
@@ -2,12 +2,22 @@
 EnemySkill = {
   name = "いてつくはどう",
   performSkill = function(self, user, characterManager, enemyManager)
+    local key = "tensionWave"
+    local turn = user:getRegister(key, 0)
+    local mManager = MessageManager:sharedManager() -- メッセージマネージャーを取り出す
+    if turn == 0 then
       local message = user:getName().."の　ゆびさきから　はどうが　ほとばしる"
-      local mManager = MessageManager:sharedManager() -- メッセージマネージャーを取り出す
       mManager:pushMessage(message)
+      user:setRegister(key, 1)
+    elseif turn == 1 then
       local message2 = characterManager:getCurrentCharacter():getName().."の　テンションが　もとにもどってしまった！"
       mManager:pushMessage(message2)
+      local layer = EffectLayer:sharedLayer()
+      layer:addEffectOnEnemy(nil, "run", 3, CCRectMake(0, 0, 480, 320))
+      characterManager:setRepeatCount(0)
       characterManager:resetTension() -- テンションを0にする
+      user:setRegister(key, 0) -- 溜めターンを0にする
+    end
   end
 }
 return EnemySkill
