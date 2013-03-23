6a2c893859529d292aad543c6fbd69e5cf18132c
giginet <giginet.net@gmail.com>
2013-03-23 11:56:53 +0900

    チュートリアルwait差し替え。草原、サイバー面に仮背景追加

--- VOXCHRONICLE/Resources/Script/debug.lua
+++ VOXCHRONICLE/Resources/Script/debug.lua
@@ -1,15 +1,15 @@
 DebugData = {
   stages = {
     -- デバッグメニューに表示するステージ名のリストを定義します
-    "1a_simple",
-    "1b_field",
-    "3a_ruin",
+    "2a_forest",
     "2c_cyber",
+    "3a_ruin",
+    "3b_castle",
     "3c_ocean",
+    "3d_space",
     "ruin_boss",
     "bossdebug",
     "ocean_boss",
-    "ttn",
   }
 }
 
