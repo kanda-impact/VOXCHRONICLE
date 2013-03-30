98236c795bc4cbfb251d723f83cdb3647c5525f7
wwwpaper <oniok@hotmail.co.jp>
2013-03-30 17:18:50 +0900

    炭鉱を作成

---
+++ VOXCHRONICLE/Resources/Script/enemies/mukade2B0.lua
@@ -0,0 +1,12 @@
+Enemy = {
+  species = "mukade",
+  level = 10,
+  hp = 16,
+  getExp = function(default)
+      return 24
+  end,
+  item = EnemyItemNone,
+  type = SkillTypeNormal,
+}
+
+return Enemy
