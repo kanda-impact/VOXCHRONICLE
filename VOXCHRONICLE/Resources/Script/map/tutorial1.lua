3eadd4e235ff29535e704cce3384340a2436b157
miio <info@miio.info>
2013-04-07 01:17:10 +0900

    Lv11チュートリアルポップアップの実装

--- VOXCHRONICLE/Resources/Script/map/tutorial1.lua
+++ VOXCHRONICLE/Resources/Script/map/tutorial1.lua
@@ -12,27 +12,16 @@ Map = {
     return {slime0 = 1}
   end,
   onLevelUp = function(self, characterManager, enemyManager)
+    self.__IRegister__:clearRegister()
     local level = characterManager:getLevel()
-    if level == 1 then
-      local layer = EffectLayer:sharedLayer()
-      local popup = layer:addPopupWindow(3)
-      local node = popup:getPage(0)
-      print(node)
-      local dimensions = CCSizeMake(380, 280)
-      node:setAnchorPoint(ccp(0.5, 0.5))
-      local hoge = [[
-さあ！攻撃だ！
-剣のコマンドを押すんだ！ざしゅ！！攻撃キター！！！！こうげききたー！
-みろよ！スライムが！無残に粉砕！ひゃっほう。
-      ]]
-      local text = CCLabelTTF:create(hoge, "Helvetica", 22, dimensions, kCCTextAlignmentLeft, kCCVerticalTextAlignmentTop)
-      local shadow = CCLabelTTF:create(hoge, "Helvetica", 22, dimensions, kCCTextAlignmentLeft, kCCVerticalTextAlignmentTop)
-      text:setColor(ccc3(220, 220, 220))
-      shadow:setColor(ccc3(33, 33, 33))
-      text:setPosition(ccp(200, 150))
-      shadow:setPosition(ccp(202, 148))
-      node:addChild(shadow)
-      node:addChild(text)
+    local layer = EffectLayer:sharedLayer()
+    if level == 11 then
+      local popup = layer:addPopupWindow(1)
+      popup:setText(0, "ラスカにチェンジ！", [[
+さぁ！ラスカちゃんに注目が集まるチュートリアルBよ！そろそろオクス君も疲れたでしょうから、私とタッチ交代してみて。新しくチェンジコマンドが追加されてるからタッチして私に交代してちょうだい。音楽も変わって気分一新よ！
+
+私、ラスカは魔法使い。オクス君は剣だけど私は杖で氷結魔法を使って攻撃するわ。基本はオクス君と操作は変わらないから色々試してみてね！
+]])
     end
   end,
   getEnemyPopRate = function(level)
