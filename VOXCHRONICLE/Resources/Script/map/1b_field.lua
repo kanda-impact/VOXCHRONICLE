0670626e83526727ecfb7e14e2f6ed6a70f0ffc0
wwwpaper <oniok@hotmail.co.jp>
2013-04-06 15:17:56 +0900

    草原作成途中 enemy作ってないので読めない

--- VOXCHRONICLE/Resources/Script/map/1b_field.lua
+++ VOXCHRONICLE/Resources/Script/map/1b_field.lua
@@ -9,14 +9,33 @@ Map = {
   initialLevel = 1,
   maxLevel = 10,
   getEnemyTable = function(level)
-    if level <= 5 then
-      return {flower1B0 = 0}
-    else
-      return {slime0 = 1}
+    if level <= 2 then
+      return {slime1B7 = 3, flower1B2 = 0, moth1B0 = 2, hornet1B2 = 0}
+    elseif level <= 4 then
+      return {slime1B7 = 0, flower1B2 = 1, moth1B0 = 5, hornet1B2 = 0}
+    elseif level <= 6 then
+      return {slime1B7 = 0, flower1B2 = 1, moth1B0 = 0, hornet1B2 = 1}
+    elseif level <= 8 then
+      return {slime1B7 = 1, flower1B2 = 1, moth1B0 = 2, hornet1B2 = 0}
+    elseif level <= 10 then
+      return {slime1B7 = 1, flower1B2 = 1, moth1B0 = 1, hornet1B2 = 1}
+    end
+      return {slime1B7 = 1, flower1B2 = 1, moth1B0 = 1, hornet1B2 = 1}
     end
   end,
   getEnemyPopRate = function(level)
-    return 0.7
+    if level <= 2 then
+    return 0.3
+    elseif level <= 4 then
+    return 0.4
+    elseif level <= 6 then
+    return 0.3
+    elseif level <= 8 then
+    return 0.3
+    elseif level <= 10 then
+    return 0.3
+    end
+    return 0.2
   end,
   fixedEnemies = {
     {"flower1B0",0}
