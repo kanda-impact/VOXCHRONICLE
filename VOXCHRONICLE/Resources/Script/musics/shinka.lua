9833eab2b47ccfddb0ba8cd45f49a0c6dc4a0356
giginet <giginet.net@gmail.com>
2013-04-08 20:47:15 +0900

    森林面実装

---
+++ VOXCHRONICLE/Resources/Script/musics/shinka.lua
@@ -0,0 +1,22 @@
+Music = {
+  name = "メイガラの森",
+  composer = "shinka_cb",
+  prefix = "2a",
+  type = MusicSetTypeWay,
+  introCount = 2,
+  finishCount = 4,
+  waitCount = 1,
+  common = {
+    wait = false,
+    shield = false,
+    run = false,
+    tension = false,
+    miss = false,
+    drum = true,
+    impact = true
+  },
+  ignoreDrums = {
+  }
+}
+
+return Music
