a7a45cc473172185c37c1d1d2ff1923b65f97e68
giginet <giginet.net@gmail.com>
2013-04-10 21:30:26 +0900

    TestFlighWrapper作ってるよ

---
+++ VOXCHRONICLE/Classes/Util/TestFlightWrapper.cpp
@@ -0,0 +1,9 @@
+//
+//  TestFlightWrapper.cpp
+//  VOXCHRONICLE
+//
+//  Created by giginet on 2013/4/9.
+//
+//
+
+#include "TestFlightWrapper.h"
---
+++ VOXCHRONICLE/Classes/Util/TestFlightWrapper.h
@@ -0,0 +1,29 @@
+//
+//  TestFlightWrapper.h
+//  VOXCHRONICLE
+//
+//  Created by giginet on 2013/4/9.
+//
+//
+
+#ifndef __VOXCHRONICLE__TestFlightWrapper__
+#define __VOXCHRONICLE__TestFlightWrapper__
+
+#include <iostream>
+
+#import "TestFlight.h"
+
+namespace TestFlight {
+  class TestFlight {
+   public:
+    static void addCustomEnvironmentInformation(const char* information, const char* key);
+    static void takeOff(const char* teamToken);
+    static void setOptions(map<string, bool>)options;
+    static void passCheckpoint(const char* checkpointName);
+    static void openFeedbackView();
+    static void submitFeedback(const char* feedback);
+    static void setDeviceIndetifier(const char* deviceIdentifier);
+  };
+};
+
+#endif /* defined(__VOXCHRONICLE__TestFlightWrapper__) */
