09be34c8b0d06f020da51df121bfde88793f5a27
giginet <giginet.net@gmail.com>
2013-04-15 20:58:11 +0900

    画像圧縮再び

--- VOXCHRONICLE/Resources/graphic_converter.py
+++ VOXCHRONICLE/Resources/graphic_converter.py
@@ -1,9 +1,17 @@
 import os
+import sys
+import commands
 from PIL import Image
 
-SRC = "Retina"
-DST = "Image"
+SRC = "Original"
+DSTS = (
+    ("Image", (0.5, 0.5)),
+    ("Retina", (1.0, 1.0))
+)
 IGNORES = ['cyber_default', 'cave_default', 'grass_default']
+COMPRESS_COMMAND = 'pngquant --speed %(speed)d --quality=%(quality)s --force --ext %(ext)s %(src)s'
+SPEED = 1
+QUALITY = '50-80'
 
 def remove_all(path):
     for (root, dirs, files) in os.walk(path, topdown=False):
@@ -20,13 +28,26 @@ def remove_all(path):
     os.rmdir(path)
 
 if __name__ == '__main__':
+    compress = False
+    if len(sys.argv) > 1 and sys.argv[1] == '--compress': compress = True
     files = os.listdir(SRC)
     base = os.getcwd()
     for file in files:
         filename, ext = os.path.splitext(file)
-        print filename
         if ext == '.png' and not filename in IGNORES:
+            print filename
             original = Image.open(os.path.join(base, SRC, file))
             width, height = original.size
-            tmb = original.resize((width / 2, height / 2), Image.ANTIALIAS)
-            tmb.save(os.path.join(base, DST, '%s%s' % (filename, ext)))
+            for dst, scale in DSTS:
+                if int(scale[0]) != 1 or int(scale[1]) != 1:
+                    img = original.resize((int(width * scale[0]), int(height * scale[1])), Image.ANTIALIAS)
+                else:
+                    img = original
+                img.save(os.path.join(dst, '%s%s' % (filename, ext)))
+                if compress:
+                    print commands.getoutput(COMPRESS_COMMAND % {'quality' : QUALITY, 
+                        'speed' : SPEED, 
+                        'src' : os.path.join(base, dst, file), 
+                        'ext' : ext
+                    })
+
