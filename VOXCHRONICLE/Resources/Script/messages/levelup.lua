4d4c6293789a549e486d552f3df6b81eaa06ce00
giginet <giginet.net@gmail.com>
2013-04-29 03:51:23 +0900

    メッセージの表示位置を中央揃えに close #99

--- VOXCHRONICLE/Resources/Script/messages/levelup.lua
+++ VOXCHRONICLE/Resources/Script/messages/levelup.lua
@@ -2,8 +2,8 @@ return function(map, characterManager, enemyManager)
   local chara = characterManager:getCurrentCharacter():getName()
   --Ｌｖアップの時
 return {
-    "${chara}　レベルアップ！",
-    "${chara}の　レベルが　あがった",
+    string.format("%s　レベルアップ！", chara),
+    string.format("%sの　レベルが　あがった", chara),
     "けいけんちが　たまった",
     "レベルアップ　したわよ！",
     "レベルあーーっぷ",
