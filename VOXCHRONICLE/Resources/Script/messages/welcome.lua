fff63d4d8edabab8f415194a789fdbcb54e46297
miio <info@miio.info>
2013-04-29 03:27:07 +0900

    welcomeメッセージ 仮実装

--- VOXCHRONICLE/Resources/Script/messages/welcome.lua
+++ VOXCHRONICLE/Resources/Script/messages/welcome.lua
@@ -1,8 +1,12 @@
+require("concat")
+require("timezone")
 return function(map, characterManager, enemyManager)
   local chara = characterManager:getCurrentCharacter():getName()
---開始時メッセージ関係
---ステージ開始時共通
-return {
+  local mapId = map:getIdentifier()
+  local currentTimeZone = getCurrentTimeZone()
+  --開始時メッセージ関係
+  --ステージ開始時共通
+  local commonPattern = {
     "さあ　てきだ！",
     "モンスターが　あらわれた",
     "モンスターだ　どうする？　こまんど▼",
@@ -11,8 +15,9 @@ return {
     "まものの　においがする．．．",
     "てきがくるぞ　きをつけろ",
   }
---[[
---ステージ開始時1a
+  return concat(commonPattern, (function()
+    --ステージ開始時1a
+    if mapId == "1a_simple" then
 return {
     "コマンドを　タッチして　たたかおう！",
     "さあ！　たたかいの　はじまりだ",
@@ -21,7 +26,10 @@ return {
     "ぼうけんの　たびに　しゅっぱつ",
     "けんをとれ　たたかいが　はじまるぞ",
   }
---ステージ開始時1b昼
+    end
+
+    --ステージ開始時1b昼
+    if mapId == "1b_field" and currentTimeZone == 0 then
 return {
     "さあ！　たたかいの　はじまりだ",
     "ぼうけんが　いま　はじまる",
@@ -34,14 +42,20 @@ return {
     "",
     "さいしょからクライマックスだぜ！",
   }
---ステージ開始時2a
+    end
+
+    --ステージ開始時2a
+    if mapId == "2a_forest" then
 return {
     "ここは．．．しんりんね",
     "もりに　とうちゃくしたようだ",
     "しんりんの　たたかいが　はじまる",
     "このもりの　なは　ガメイラ",
   }
---ステージ開始時2b
+    end
+
+    --ステージ開始時2b
+    if mapId == "2b_cave" then
 return {
     "すっかり　さびれてしまっている",
     "",
@@ -49,7 +63,10 @@ return {
     "あかりが　ついてて　よかった",
     "",
   }
---ステージ開始時2c
+    end
+
+    --ステージ開始時2c
+    if mapId == "2c_cyber" then
 return {
     "ここは　どこなの．．．！？",
     "――電脳世界　ファミル――",
@@ -60,7 +77,9 @@ return {
     "なつかしい　かおりがする",
     "レトロ　ね．．．",
   }
---ステージ開始時3a
+    end
+    --ステージ開始時3a
+    if mapId == "3a_ruin" then
 return {
     "",
     "グランエターナは　このさきに...?",
@@ -68,15 +87,21 @@ return {
     "",
     "",
   }
---ステージ開始時3b
-return {
-    "",
-    "",
-    "",
-    "",
-    "",
-  }
---ステージ開始時3c
+    end
+
+    --ステージ開始時3b
+    if mapId == "3b_castle" then
+      return {
+        "",
+        "",
+        "",
+        "",
+        "",
+      }
+    end
+
+    --ステージ開始時3c
+    if mapId == "3c_ocean" then
 return {
     "",
     "",
@@ -84,7 +109,10 @@ return {
     "",
     "しゅうちゅうしゅうちゅう　だいじょうぶだいじょうぶ",
   }
---ステージ開始時3d
+    end
+
+    --ステージ開始時3d
+    if mapId == "3d_space" then
 return {
     "ここは惑星外宙域　Ｓ－Ｂ－α",
     "しょあくの　こんげんが　ここに．．．？",
@@ -92,7 +120,10 @@ return {
     "ここは　うちゅう．．．",
     "ラスカ　おまえはいったい―――！？",
   }
---ステージ開始時1b夕
+    end
+
+    --ステージ開始時1b夕
+    if mapId == "1b_field" and currentTimeZone == 1 then
 return {
     "さあ！　たたかいの　はじまりだ",
     "ぼうけんが　いま　はじまる",
@@ -105,7 +136,10 @@ return {
     "",
     "",
   }
---ステージ開始時1b夜
+    end
+
+    --ステージ開始時1b夜
+    if mapId == "1b_field" and currentTimeZone == 2 then
 return {
     "さあ！　たたかいの　はじまりだ",
     "ぼうけんが　いま　はじまる",
@@ -118,6 +152,7 @@ return {
     "",
     "",
   }
-end
-  ]]--
+    end
+  end
+  )())
 end
