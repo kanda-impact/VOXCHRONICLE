f3953ec49f0deafa8dc8338f9ebb4f9767ec9536
wwwpaper <oniok@hotmail.co.jp>
2013-05-05 00:01:14 +0900

    波の出現位置調整 close #264

--- VOXCHRONICLE/Resources/Script/enemySkills/wave_skill.lua
+++ VOXCHRONICLE/Resources/Script/enemySkills/wave_skill.lua
@@ -4,7 +4,7 @@ EnemySkill = {
   performSkill = function(self, user, characterManager, enemyManager)
     row = user:getRow()
     math.random(100)
-    col = math.random(3)
+    col = math.random(3) - 1
     SimpleAudioEngine:sharedEngine():playEffect("enemy_wave.mp3")
     MessageManager:sharedManager():pushMessage(user:getName().."は　つなみを　おこした！")
     enemyManager:popEnemyAt("wave3C0", row, col) -- 波を3つ生成する 
