52d3798dfc076a53ada30df779653562bafabf58
TTNTAWASi <last-demise@jcom.home.ne.jp>
2013-05-05 11:54:21 +0900

    細かい文章の改修

--- VOXCHRONICLE/Resources/Script/enemySkills/laugh.lua
+++ VOXCHRONICLE/Resources/Script/enemySkills/laugh.lua
@@ -3,7 +3,7 @@ EnemySkill = {
   name = "笑う",
   performSkill = function(self, user, characterManager, enemyManager)
     local mManager = MessageManager:sharedManager()
-    mManager:pushMessage(user:getName().."は　不適な　笑みを　浮かべている")
+    mManager:pushMessage(user:getName().."は　不敵な　笑みを　浮かべている")
     --(user:getName().."は　ふしぎな　おどりを　おどった")
     --(user:getName().."は　にやにや　している")
     --(user:getName().."は　こちらの　ようすを　うかがっている")
